28ec2c204eee0a698838e61b514d25e85aa7adba
aiappsy <paljuritzen@gmail.com>
2025-12-10 07:26:18 +0100

    Implement Editor component layout with Sidebar, Canvas, and TreeView

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,1 +1,31 @@
 
+// /src/components/editor/Editor.tsx
+
+import React from "react";
+import Sidebar from "./Sidebar";
+import Canvas from "./Canvas";
+import Toolbar from "./Toolbar";
+import TreeView from "./TreeView";
+
+export default function Editor() {
+  return (
+    <div className="flex h-screen w-full bg-gray-50 text-gray-900">
+      
+      {/* Left Sidebar */}
+      <div className="w-64 border-r bg-white p-4">
+        <Sidebar />
+      </div>
+
+      {/* Center Canvas */}
+      <div className="flex-1 flex flex-col">
+        <Toolbar />
+        <Canvas />
+      </div>
+
+      {/* Right Tree View */}
+      <div className="w-64 border-l bg-white p-4">
+        <TreeView />
+      </div>
+    </div>
+  );
+}
